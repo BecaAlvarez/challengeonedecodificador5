e7d8b1c57531e78794b3e4d70cb285237ba90287
Rebeca Alvarez <63008311+BecaAlvarez@users.noreply.github.com>
2023-05-22 09:00:16 -0300

    :lipstick: :hammer: Ui e Code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const textArea1 = document.querySelector(".areaTexto1");
 const textArea2 = document.querySelector(".areaTexto2");
 //
-const btnCopiar = document.querySelector(".btn-copiar");
+const btnCopy = document.querySelector(".btn-copiar");
 
 
 // A letra "e" é convertida para "enter"
@@ -12,55 +12,71 @@ const btnCopiar = document.querySelector(".btn-copiar");
 
 function btnEncriptar(){
     const textoEncriptado = encriptar(textArea1.value);
-    textArea2.value = textoEncriptado;
-    return
+
+    if(textoEncriptado == ''){
+        alert('Campo vazio. Digite o texto a ser criptografado')
+    }else{
+        textArea2.value = textoEncriptado;
+        return
+    }
+    
 }
 
 
-function encriptar(stringEncript){
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncript = stringEncript.toLowerCase();
+function encriptar(encryptedText){
+    let arrayCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    encryptedText = encryptedText.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncript.includes(matrizCodigo[i][0])){
-            stringEncript = stringEncript.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    for(let i = 0; i < arrayCode.length; i++){
+        if(encryptedText.includes(arrayCode[i][0])){
+            encryptedText = encryptedText.replaceAll(arrayCode[i][0], arrayCode[i][1]);
         }
     }
 
-    return stringEncript;
+    return encryptedText;
 }
 
 function btnDesencriptar(){
-    const textoDesencriptado = desencriptar(textArea1.value);
-    textArea2.value = textoDesencriptado;
-    return
+    const decryptedText = decrypt(textArea1.value);
+    
+    if(decryptedText == ''){
+        alert('Campo vazio. Digite o texto a ser descriptografado')
+    }else{
+        textArea2.value = decryptedText;
+        return
+    }
 
 }
 
 
+function decrypt(decryptedText){
+    let arrayCode = [["e" , "enter"], ["i" ,"imes"], ["a" , "ai"], ["o" , "ober"], ["u", "ufat"]];
+    decryptedText =  decryptedText.toLowerCase();
 
-function desencriptar(stringDesencriptado){
-    let matrizVogais = [["e" , "enter"], ["i" ,"imes"], ["a" , "ai"], ["o" , "ober"], ["u", "ufat"]];
-    stringDesencriptado =  stringDesencriptado.toLowerCase();
-
-    for(let i = 0; i < matrizVogais.length; i++){
-        if(stringDesencriptado.includes(matrizVogais[i][1])){
-           stringDesencriptado = stringDesencriptado.replaceAll(matrizVogais[i][1], matrizVogais[i][0]);
+    for(let i = 0; i < arrayCode.length; i++){
+        if(decryptedText.includes(arrayCode[i][1])){
+           decryptedText = decryptedText.replaceAll(arrayCode[i][1], arrayCode[i][0]);
         }
     }
-    return stringDesencriptado;
+    return decryptedText;
 }
 
 
 //Copiar
 function copyContent() {
     var copiedText = textArea2.value;
+
+    if(copiedText == ''){
+        alert('Campo vazio. Não há teto para ser copiado')
+    }else{
+        navigator.clipboard.writeText(copiedText).then(function(){
+            alert('Texto copiado com sucesso');
+        }).catch(function(err){
+            alert('Falha ao copiar o conteúdo', err);
+        });
+    }
     
-    navigator.clipboard.writeText(copiedText).then(function(){
-        alert('Texto copiado com sucesso');
-    }).catch(function(err){
-        alert('Falha ao copiar o conteúdo');
-    });
+    
 }
 
 
